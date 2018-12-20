5db85f989c6a54d859199ac0d20ab98c72e274d9
knazeri <kamyar.nzr@gmail.com>
2018-12-20 00:43:55 -0500

    download bash script

---
+++ scripts/download_model.sh
@@ -0,0 +1,14 @@
+DIR='./checkpoints'
+URL='https://drive.google.com/uc?export=download&id=13G_Bszli37e4An-owPLXSBsayVafqfGX'
+
+echo "Downloading pre-trained models..."
+mkdir -p $DIR
+FILE="$(curl -sc /tmp/gcokie "${URL}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')" 
+curl -Lb /tmp/gcokie "${URL}&confirm=$(awk '/_warning_/ {print $NF}' /tmp/gcokie)" -o "$DIR/${FILE}" 
+
+echo "Extracting pre-trained models..."
+cd $DIR
+unzip $FILE
+rm $FILE
+
+echo "Download success."
