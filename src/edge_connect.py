0419ed6d1ad5980accbd75720bb6b1fca62b5311
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-13 20:22:29 -0700

    Fixed PYTHON_F401 issues in src/edge_connect.py

--- src/edge_connect.py
+++ src/edge_connect.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 import torch
-import torch.nn as nn
 from torch.utils.data import DataLoader
 from .dataset import Dataset
 from .models import EdgeModel, InpaintingModel
