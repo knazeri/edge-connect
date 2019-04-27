369c49394ef6652db2c663cc6e2fb3616976f9bf
knazeri <kamyar.nzr@gmail.com>
2019-04-27 17:09:35 -0400

    fix flist with invalid image file extensions #82

--- scripts/flist.py
+++ scripts/flist.py
@@ -7,13 +7,13 @@ parser.add_argument('--path', type=str, help='path to the dataset')
 parser.add_argument('--output', type=str, help='path to the file list')
 args = parser.parse_args()
 
-ext = {'.jpg', '.png'}
+ext = {'.JPG', '.JPEG', '.PNG', '.TIF', 'TIFF'}
 
 images = []
 for root, dirs, files in os.walk(args.path):
     print('loading ' + root)
     for file in files:
-        if os.path.splitext(file)[1] in ext:
+        if os.path.splitext(file)[1].upper() in ext:
             images.append(os.path.join(root, file))
 
 images = sorted(images)
